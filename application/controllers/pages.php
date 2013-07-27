1594d9e0d82fcd98f828288a6146113abe46e83f
odessy <odessymark@gmail.com>
2013-07-26 22:23:36 -0500

    added pages controller example

---
+++ application/controllers/pages.php
@@ -0,0 +1,12 @@
+<?php
+	class Home extends Controller
+	{
+		function homePage( $args = false ) {
+			//$this->setview("page/home.php");
+			print "This is the home page.";
+		}
+		
+		function aboutPage( $args = false ) {
+			print "This is the about page.";
+		}
+	}

5da27cef046d8b4f85e828eaf9db4e1e73be0c17
odessy <odessymark@gmail.com>
2013-07-26 21:42:59 -0500

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+  function autoload_classes($class_name)
+	{
+		$file = realpath(dirname(__FILE__) .'/includes/class.' . strtolower($class_name). '.php';
+		if (file_exists($file))
+		{
+			require_once($file);
+		}
+	}
+
+	function autoload_controllers($class_name)
+	{
+		$file = APPLICATION_ROOT.'/controllers/' . strtolower($class_name). '.php';
+		if (file_exists($file))
+		{
+			require_once($file);
+		}
+	}
+
+	spl_autoload_register('autoload_classes');
+	spl_autoload_register('autoload_controllers');

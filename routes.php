2926603846cbbacee05ba6d112f379de70a2a7a0
odessy <odessymark@gmail.com>
2013-07-26 21:47:58 -0500

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,16 @@
+<?php
+  /**
+	*  routes in $routes array variable
+	*/
+	
+	//example
+	// $routes = array(
+		// ""  => array( "EVENT", "index" ),
+		// "index/user"  => array( "USERS", "index" ),
+		// "about"  => array( "HOME", "aboutPage" ),
+		// "home" => array("HOME", "homePage"),
+	// );
+	
+	$routes = array(
+		//place routes here
+	);
